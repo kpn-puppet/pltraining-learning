f10b3062a84f6dc5b3f6f32d5f21d3691a0fcf15
Rob Heijkoop <rob.heijkoop@kpn.com>
2025-11-09 18:00:46 +0100

    fix packages and files

--- manifests/quest_guide.pp
+++ manifests/quest_guide.pp
@@ -1,6 +1,6 @@
 class learning::quest_guide (
-  $git_branch = 'master',
-  $content_repo_owner = 'cjswart',
+  $git_branch = 'main',
+  $content_repo_owner = 'kpn-puppet',
   $content_repo_name  = 'puppet-quest-guide',
 ) {
 
